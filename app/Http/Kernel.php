0a46ba8a46ee92e425b36bc9a239b37f14f300ac
kautsaralbana <kautsaralbaaa@gmail.com>
2019-10-22 01:43:31 +0700

    Config Role Permission

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class, // spatie/laravel-permissions
+	    'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class, // spatie/laravel-permissions
     ];
 
     /**
